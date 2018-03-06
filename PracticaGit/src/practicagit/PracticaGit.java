f0e870d89ee11f89ea6cd1b584fb2f1937e7e422
alumno <alumno@aula01-04>
2018-03-06 19:40:03 +0100

    Objeto de la clase creado y probado

--- PracticaGit/src/practicagit/PracticaGit.java
+++ PracticaGit/src/practicagit/PracticaGit.java
@@ -19,6 +19,8 @@ public class PracticaGit {
         
         
         // 2º Crea obetos con las clases nuevas y utilizalas
+        Andres andres = new Andres();
+        System.out.println(andres.toString());
     }
     
 }
