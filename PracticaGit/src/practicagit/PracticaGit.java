627b6353714db4cda2d6b536a7cbaa715fb78c79
Profesor <Profesor@Ternasco>
2018-03-06 10:52:08 +0100

    Inicio práctica GitHub

---
+++ PracticaGit/src/practicagit/PracticaGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicagit;
+
+/**
+ *
+ * @author Juan Francisco Zamora Agudo
+ */
+public class PracticaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 1º Crea una clase con tu nombre
+        
+        
+        // 2º Comming soon
+    }
+    
+}
